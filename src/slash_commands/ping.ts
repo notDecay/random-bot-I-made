0b345803d04fb69de205f456708f0f3e79bf7d54
notDecay <thisisjustalongtext@gmail.com>
2024-04-22 20:22:19 +0700

    /ping: remove unnecessary things

--- src/slash_commands/ping.ts
+++ src/slash_commands/ping.ts
@@ -1,20 +1,25 @@
-import { type ISlashCommand, formatEmbedDate, Color } from "../utils"
+import { 
+  type ISlashCommand, 
+  formatEmbedDate, 
+  Color 
+} from "../utils"
 
 export const ping = {
   name: 'ping',
   description: 'Check if I am stil alive or not',
-  async run({ interaction, client, args }) {
+  async run({ interaction, client }) {
     await interaction.deferReply({
       ephemeral: true
     })
+
+    const upTimeInMinutes = (client.uptime ?? 0) / 1000 / 60
 
     await interaction.followUp({
       embeds: [{
         title: ':ping_pong: Pong!',
         description: [
           `> - Websocket ping: \` ${client.ws.ping} \` miliseconds`,
-          `> - Still quackin' for ${client.uptime ?? 0 / 1000} seconds`,
-          `> - Watching <@894899742822252556> until he embarrassed (plz don't ask \*smilly face*)`
+          `> - Being alive for \` ${upTimeInMinutes} \` minutes`,
         ].join('\n'),
         timestamp: formatEmbedDate(),
         color: Color.NO_COLOR
